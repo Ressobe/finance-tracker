835af1664ba3da92a995468b7d2064c970a3fe5a
Bartosz Sobina <bartosz.sobina44@gmail.com>
2024-11-29 11:41:31 +0100

    fix: add required to attributes of TotalFlowSummaryDto

--- backend/Dtos/Transaction/TotalFlowSummaryDto.cs
+++ backend/Dtos/Transaction/TotalFlowSummaryDto.cs
@@ -1,14 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace api.Dtos.Transaction
 {
   public class TotalFlowSummaryDto
   {
+    [Required]
     public decimal TotalFlow { get; set; }
+    [Required]
     public decimal TransactionTotal { get; set; }
+    [Required]
     public decimal TransferTotal { get; set; }
+    [Required]
     public decimal SavingTotal { get; set; }
 
+    [Required]
     public decimal TransactionPercentage { get; set; }
+    [Required]
     public decimal TransferPercentage { get; set; }
+    [Required]
     public decimal SavingPercentage { get; set; }
   }
 }
